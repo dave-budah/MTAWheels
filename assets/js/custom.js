163480784c95341c1ecfca240cedf69f37e88ff4
johnmanda <89446442+johnmanda@users.noreply.github.com>
2022-03-11 14:06:33 +0200

    Toggle added.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -55,3 +55,67 @@ function  company() {
     y.style.left = '-600px';
     z.style.left = '0px';
 }
+
+// toggle password
+function togglePW(){
+    var password = document.querySelector('[name=password]');
+
+    if(password.getAttribute('type') === 'password') {
+        password.setAttribute('type', 'text');
+        document.getElementById('review').classList.remove('fa-eye')
+        document.getElementById('review').classList.add('fa-eye-slash')
+    }
+    else {
+        password.setAttribute('type', 'password');
+        document.getElementById('review').classList.remove('fa-eye-slash')
+        document.getElementById('review').classList.add('fa-eye')
+    }
+}
+
+// toggle password
+function togglePWD(){
+    var confirmPassword = document.querySelector('[name=confirmPassword]');
+
+    if(confirmPassword.getAttribute('type') === 'password') {
+        confirmPassword.setAttribute('type', 'text');
+        document.getElementById('reviewer').classList.remove('fa-eye')
+        document.getElementById('reviewer').classList.add('fa-eye-slash')
+    }
+    else {
+        confirmPassword.setAttribute('type', 'password');
+        document.getElementById('reviewer').classList.remove('fa-eye-slash')
+        document.getElementById('reviewer').classList.add('fa-eye')
+    }
+}
+
+// toggle password
+function togglePWDI(){
+    var ipassword = document.querySelector('[name=i-password]');
+
+    if(ipassword.getAttribute('type') === 'password') {
+        ipassword.setAttribute('type', 'text');
+        document.getElementById('i-review').classList.remove('fa-eye')
+        document.getElementById('i-review').classList.add('fa-eye-slash')
+    }
+    else {
+        ipassword.setAttribute('type', 'password');
+        document.getElementById('i-review').classList.remove('fa-eye-slash')
+        document.getElementById('i-review').classList.add('fa-eye')
+    }
+}
+
+// toggle password
+function toggleIPWD(){
+    var confirmPasswordI = document.querySelector('[name=passwordConfirm]');
+
+    if(confirmPasswordI.getAttribute('type') === 'password') {
+        confirmPasswordI.setAttribute('type', 'text');
+        document.getElementById('i-reviewer').classList.remove('fa-eye')
+        document.getElementById('i-reviewer').classList.add('fa-eye-slash')
+    }
+    else {
+        confirmPasswordI.setAttribute('type', 'password');
+        document.getElementById('i-reviewer').classList.remove('fa-eye-slash')
+        document.getElementById('i-reviewer').classList.add('fa-eye')
+    }
+}
